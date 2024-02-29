e09976f8c829cabef986d77e279ad898db028856
moo3030 <mohamedabdelhamid3030@gmail.com>
2024-03-01 01:04:07 +0200

    remove unused code

--- src/utils.py
+++ src/utils.py
@@ -5,10 +5,9 @@ import threading
 import psutil
 import torch as T
 import time
-import shutil
 import numpy as np
 from pathlib import Path
-from typing import Dict, Union, Callable
+from typing import Dict, Union
 from config import paths
 
 
@@ -106,88 +105,10 @@ def get_model_parameters(
     return hyperparameters
 
 
-def is_segment_in_path(segment: str, path: str) -> bool:
-    """
-    Check if the specified segment is a part of the given path.
-
-    Args:
-    - segment (str): The directory segment to check.
-    - path (str): The full path to check against.
-
-    Returns:
-     - bool: True if the segment is part of the path, False otherwise.
-    """
-    # Create a Path object for the path
-    path_obj = Path(path)
-
-    # Iterate through each part of the path to check if the segment exists
-    return segment in path_obj.parts
-
-
-def replace_segment_in_path(
-    original_path: str, old_segment: str, new_segment: str
-) -> str:
-    """
-    Replace an old segment with a new segment in the given path.
-
-    Args:
-    - original_path (str): The original path as a string.
-    - old_segment (str): The path segment to be replaced.
-    - new_segment (str): The new segment to replace the old one.
-
-    Returns:
-    - str: The modified path with the segment replaced.
-    """
-    # Break down the original path into its components
-    parts = list(Path(original_path).parts)
-
-    # Replace the old segment with the new segment where found
-    modified_parts = [new_segment if part == old_segment else part for part in parts]
-
-    # Reconstruct the path from the modified parts
-    # Use Path() to handle different OS path separators automatically
-    modified_path = Path(*modified_parts)
-
-    # Convert the Path object back to a string representation
-    return str(modified_path)
-
-
-def move_file(source_path: str, destination_path: str) -> None:
-    """
-    Move a file from a source path to a destination path.
-
-    This function moves a file from the specified source path to the specified destination path.
-    If the directory structure for the destination path does not exist, it is created.
-
-    Parameters:
-    - source_path (str): The path to the file that needs to be moved. This should include the full
-      path along with the file name.
-    - destination_path (str): The target path where the file should be moved. This includes the full
-      path along with the new file name at the destination.
-
-    Returns:
-    None
-
-    """
-    os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-    shutil.move(source_path, destination_path)
-
-
 def contains_subdirectories(folder_path):
     """Check if the given folder contains any subdirectories using pathlib."""
     folder = Path(folder_path)
     return any(entry.is_dir() for entry in folder.iterdir())
-
-
-def list_paths(root_dir):
-    """lists all dirs/file paths in a given directory."""
-    paths = []
-    listdir = [i for i in os.listdir(root_dir)]
-    for entry in listdir:
-        full_path = os.path.join(root_dir, entry)
-        paths.append(full_path)
-
-    return paths
 
 
 def get_optimizer(optimizer_str: str):
